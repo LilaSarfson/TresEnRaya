c33dfba4a8bc2ac77b4f583e8cea9d317160ffd3
LilaSarfson <lilamandarina@hotmail.com>
2022-06-08 13:45:51 +0200

    first commit

--- main.js
+++ main.js
@@ -1,26 +1,19 @@
 let cuadricula = document.querySelector(".main");
 let win = false;
-let cruces = [];
-let circulo = "";
+let divs = [];
 let turno = 0;
 cuadricula.addEventListener("click", function (e) {
-
     let currentTarget = e.target.getAttribute("id");
     let changeImg = document.getElementById(currentTarget);
     if (changeImg.style.backgroundImage == "") {
         if (!isPair(turno)) {
             changeImg.style.backgroundImage = "url('src/cerrar.png')";
-            cruces.push(currentTarget);
-            console.log(cruces);
             turno++;
         }
 
         else if (isPair(turno)) {
             changeImg.style.backgroundImage = "url('src/circulo.png')";
-            circulo = currentTarget + circulo;
-            console.log(circulo);
             turno++;
-            checkwin(circulo);
         }
         console.log(turno);
 
@@ -35,10 +28,4 @@ function isPair(num) {
 }
 
 function checkwin(array) {
-
-    if (array == (312) || array == (123) || array == (132) || array == (321) || array == (741) || array == (147) || array == (714) || array == (174) || array == (471) || array == (417)) {
-
-        return console.log("gane con la primera fila");
-        win = true;
-    }
 }
