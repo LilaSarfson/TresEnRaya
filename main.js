625bddf6b93e49f09c42e62381b1298ac916ff7c
Your Name <lilamandarina@hotmail.com>
2022-07-02 12:34:56 +0200

    update

--- main.js
+++ main.js
@@ -8,12 +8,12 @@ cuadricula.addEventListener("click", function (e) {
     if(!win){
         if (changeImg.style.backgroundImage == "") {
             if (!isPair(turno)) {
-                changeImg.style.backgroundImage = "url('src/cerrar.png')";
+                changeImg.style.backgroundImage = "url('./src/assets/cruz2.png')";
                 cuadrados[currentTarget].boleano = 1;
                 turno++;
             }
             else if (isPair(turno)) {
-                changeImg.style.backgroundImage = "url('src/circulo.png')";
+                changeImg.style.backgroundImage = "url('./src/assets/circulo.png')";
                 cuadrados[currentTarget].boleano = 2;
                 turno++;
             }
@@ -39,35 +39,27 @@ function isPair(num) {
 function checkwin(array, num) {
         if (array[0].boleano == num && array[1].boleano == num && array[2].boleano ==num){
             win = true;
-            console.log("Has ganado")
         }
         if(array[0].boleano == num && array[3].boleano == num && array[6].boleano == num){
             win = true;
-            console.log("Has ganado")
         }
         if(array[0].boleano == num && array[4].boleano == num && array[8].boleano == num){
             win = true;
-            console.log("Has ganado")
         }
         if(array[2].boleano == num && array[5].boleano == num && array[8].boleano == num){
             win = true;
-            console.log("Has ganado")
-        }
+               }
         if(array[2].boleano == num && array[4].boleano == num && array[6].boleano == num){
             win = true;
-            console.log("Has ganado")
         }
         if(array[6].boleano == num && array[7].boleano == num && array[8].boleano == num){
             win = true;
-            console.log("Has ganado")
         }
         if(array[3].boleano == num && array[4].boleano == num && array[5].boleano == num){
             win = true;
-            console.log("Has ganado")
         }
         if(array[1].boleano == num && array[4].boleano == num && array[7].boleano == num){
             win = true;
-            console.log("Has ganado")
         }
 }
 
@@ -83,4 +75,3 @@ addChecked(6,0);
 addChecked(7,0);
 addChecked(8,0);
 
-console.log(cuadrados);
